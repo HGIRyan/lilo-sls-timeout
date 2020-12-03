28c16145c3f5f5cde67194ab0b12cda3de985d88
Ryan Hutchison <ryanzhutch@gmail.com>
2020-12-03 13:13:07 -0700

    update

--- src/handlers/queryTimeout.js
+++ src/handlers/queryTimeout.js
@@ -1,15 +1,56 @@
-import { query, getCustomer } from "../lib";
+import moment from "moment";
+import { getCustomer, updateActivity, updateRating, updateClick } from "../lib";
+import { query } from "../lib/query";
+const { c_id, cor_id, cus_id } = process.env;
 
 const queryTimeout = async (event, context) => {
-  const c_id = 1;
-  const cor_id = 1;
-  const cus_id = 2939956;
+  const start = new Date();
+  let type;
+  if (event.pathParameters) {
+    type = event.pathParameters.type;
+  }
 
+  console.time("customerInfo");
   const customerInfo = await query(getCustomer, [cus_id]);
+  console.timeEnd("customerInfo");
+
+  let { activity } = customerInfo[0];
+
+  if (type === "rating") {
+    console.time("rating");
+    const rating = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+    activity.active.push({ date: moment().format("YYYY-MM-DD"), type: `Updating Rating to ${rating}` });
+    await query(updateRating, [cus_id, rating]);
+    console.timeEnd("rating");
+  } else if (type === "click") {
+    console.time("click");
+    const randomBool = Math.random() >= 0.5;
+    activity.active.push({ date: moment().format("YYYY-MM-DD"), type: `Updating Click to ${randomBool}` });
+    await query(updateClick, [cus_id, randomBool]);
+    console.timeEnd("click");
+  } else if (type === "clear") {
+    activity = { active: [{ date: moment().format("YYYY-MM-DD"), type: "Customer Activity Cleared" }] };
+  } else {
+    activity.active.push({ date: moment().format("YYYY-MM-DD"), type: `Type is NULL` });
+  }
+
+  console.time("updateActivity");
+  await query(updateActivity, [cus_id, activity]);
+  await query(updateActivity, [cus_id, activity]);
+  await query(updateActivity, [cus_id, activity]);
+  console.timeEnd("updateActivity");
+
+  const imports = {
+    query: query,
+    type: type,
+    totalTime: new Date() - start,
+    activityLength: activity.active.length,
+    result: customerInfo,
+  };
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "queryTimeout from Yo moms house", default: { c_id, cor_id, cus_id }, result: JSON.stringify(customerInfo[0], null, 4) }),
+    body: JSON.stringify({ message: "queryTimeout from Yo moms house", default: { c_id, cor_id, cus_id }, imports }),
   };
 };
 
--- src/lib/sqlQueries.js
+++ src/lib/sqlQueries.js
@@ -1,5 +1,28 @@
 const getCustomer = `
-SELECT * FROM CUSTOMER WHERE CUS_ID = $1;
+SELECT * FROM CUSTOMER as C
+JOIN FEEDBACK AS F ON F.CUS_ID = C.CUS_ID
+WHERE C.CUS_ID = $1;
 `;
 
-export { getCustomer };
+const updateActivity = `
+UPDATE CUSTOMER
+SET ACTIVITY = $2
+WHERE CUS_ID = $1
+RETURNING *;
+`;
+
+const updateRating = `
+UPDATE FEEDBACK 
+SET RATING = $2
+WHERE CUS_ID = $1
+RETURNING *;
+`;
+
+const updateClick = `
+UPDATE FEEDBACK
+SET CLICK = $2
+WHERE CUS_ID = $1
+RETURNING *;
+`;
+
+export { getCustomer, updateActivity, updateRating, updateClick };
